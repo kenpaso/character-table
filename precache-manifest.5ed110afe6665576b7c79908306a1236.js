f379ba97221e14494f97f9e6883d0335321539a9
kenpaso <kengalang2@gmail.com>
2020-09-28 15:01:35 -0400

    Updates

---
+++ precache-manifest.5ed110afe6665576b7c79908306a1236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f123810fee7c9f1d79e630a14cff19",
+    "url": "/character-table/index.html"
+  },
+  {
+    "revision": "b75a1c28201bc1fdbe81",
+    "url": "/character-table/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "a5d7caf5b71e76139900",
+    "url": "/character-table/static/js/2.68940474.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/character-table/static/js/2.68940474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75a1c28201bc1fdbe81",
+    "url": "/character-table/static/js/main.5ab06c97.chunk.js"
+  },
+  {
+    "revision": "0ffd98991e310d4e1ccb",
+    "url": "/character-table/static/js/runtime-main.b22cc3d7.js"
+  }
+]);
