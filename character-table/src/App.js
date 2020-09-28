600b6b03b587020718a2699973337b219c8c6d19
kenpaso <kengalang2@gmail.com>
2020-09-28 14:28:25 -0400

    added form

--- character-table/src/App.js
+++ character-table/src/App.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react'
 import Table from './Table'
-
+import Form from './Form'
 class App extends Component {
+  state = {
+    characters: [],
+  }
+
   render() {
+    const { characters } = this.state
+  
     return (
       <div className="container">
-        <Table />
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form />
       </div>
     )
+  }
+
+  removeCharacter = (index) => {
+    const {characters} = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
   }
 }
 
